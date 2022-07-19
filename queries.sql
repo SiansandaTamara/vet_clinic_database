0c7689fec3fe93d89d83154ee6c1fb278f81f013
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-07-19 13:01:57 +0200

    Add query for animals named  "Agumon" or "Pikachu"

--- queries.sql
+++ queries.sql
@@ -5,3 +5,11 @@
 
 --List the name of all animals born between 2016 and 2019.
  SELECT name, date_of_birth FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+ --List the name of all animals that are neutered and have less than 3 escape attempts.
+SELECT name,neutered, escape_attempts 
+FROM animals WHERE neutered = true 
+AND escape_attempts < 3;
+
+--List the date of birth of all animals named either "Agumon" or "Pikachu".
+SELECT name, date_of_birth FROM animals 
+WHERE name ='Agumon' OR name='Pikachu';
